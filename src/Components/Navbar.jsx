8d0116786c41b889ad1b27d1acc8af0804985da3
patrickashi <patrickpeko1@gmail.com>
2024-01-12 01:21:22 +0100

    new
    add new

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,17 +21,17 @@ const  Navbar = () => {
                 <li className="p-4">Help</li>
             </ul>
 
-            <div className="text-white mobile:hidden ">
+            <div className="text-white hidden mobile:hidden 2xl:block xl:block">
                 <button className="border right-0  py-3 font-bold  rounded-md px-11 bg-gradient-to-b from-[#047857] to bg-[#2dd4bf] text-white">Login</button>
             </div>
 
-            <div onClick={handleNav} className="mobile:block md:hidden">
+            <div onClick={handleNav} className="mobile:block md:hidden 2xl:hidden lg:hidden xl:hidden">
                 {nav ? <AiOutlineClose className="text-black" size={20}/> : <AiOutlineMenu className="text-black" size={20}/>} 
             </div>
 
             {/* toggle menu */}
             <div className={nav ? "fixed left-0 top-0 h-full w-[90%] border-r border-r-gray-900 ease-out duration-500 bg-black" : "fixed left-[-100%]"}>
-                <ul className=" p-4 my-12">
+                <ul className="p-4 my-6">
                     <li className="p-4 border-l border-blue-600">Home</li>
                     <li className="p-4 border-l border-green-600">About</li>
                     <li className="p-4 border-l border-red-600">Trade</li>
@@ -41,7 +41,7 @@ const  Navbar = () => {
 
                <div className="">
                     <button className=" my-4 w-[95%] mx-2  py-3  rounded-md px-11 bg-gradient-to-b from-[#047857] to bg-[#2dd4bf] text-white">Login</button>
-                    <button className=" w-[95%] mx-2  py-3 rounded-md px-11 bg-gradient-to-b from-[#047857] to bg-[#2dd4bf] text-white">Sign up</button>
+                    <button className=" w-[95%] mx-2  py-3 border-2 border-[#2dd4bf]  rounded-md px-11  text-[#2dd4bf]">Sign up</button>
                 </div>
             </div>
         </div>
